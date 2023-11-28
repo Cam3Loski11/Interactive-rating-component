8b7fcf6eddfedd4631aa47ebaa6f8cff1ea67b74
Juan Camelo <jfcamelo.2004@gmail.com>
2023-11-27 19:10:52 -0500

    Proyect 1

---
+++ src/main.js
@@ -0,0 +1,33 @@
+const buttons = document.querySelectorAll('.button'); // => Con querySelectorAll recupero un array de los elementos
+buttons.forEach(n => {                               //     que tengan esa clase/id/nombre
+    n.addEventListener('click', () => {
+        resetColors();
+        n.classList.add('active');
+    });
+});
+
+function resetColors() {
+    buttons.forEach(n => {
+        n.classList.remove('active');
+    });
+};
+
+const submitBtn = document.querySelector('.submitBtn');
+const mainCard = document.querySelector('.main-card');
+const resultCard = document.querySelector('.result-card');
+submitBtn.addEventListener('click', () => {
+    const selectedButton = document.querySelector('.button.active');
+
+    if (selectedButton) {
+        const selectedValue = selectedButton.value;
+        document.getElementById('result').textContent = selectedValue;
+        
+        mainCard.classList.add('inactive');
+        resultCard.classList.remove('inactive');
+    } else {
+        alert('Elige una opcion por favor')
+    }
+
+})
+
+
